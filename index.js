e23d660362220ef3d813e8ae1574c40b70339c16
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2023-09-20 12:59:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var currrentDate = new Date();
   var formattedDate = currrentDate.toDateString();
-  res.end('Horário ' + formattedDate);
+  res.end('Hello! Today is ' + formattedDate);
 }).listen(8019);
 
 
