183c5747253d7dffd5c34b3e16df10942c705530
GuilhermeVeigaPedromilo <guipedromilo@gmail.com>
2023-09-20 12:53:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,10 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  var currrentDate = new Date();
+  var formattedDate = currrentDate.toDateString();
+  res.end('Horário ' + formattedDate);
 }).listen(8019);
 
 
-function formatDate(date, format) {
-    const map = {
-        mm: date.getMonth() + 1,
-        dd: date.getDate(),
-        aa: date.getFullYear().toString().slice(-2),
-        aaaa: date.getFullYear()
-    }
 
-    return format.replace(/mm|dd|aa|aaaa/gi, matched => map[matched])
-}
